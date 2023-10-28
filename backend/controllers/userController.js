5db3dbc96155f41797d09e343f1e95fd895a14f5
PieMenard <pinedo.m.gustavo@gmail.com>
2023-10-27 20:26:24 -0400

    generate user token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,26 @@
 import asyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  if (users) res.send(users);
+  else {
+    res.status(400);
+    throw new Error('no users in database');
+  }
+});
+
+const getSingleUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  if (user) res.send(user);
+  else {
+    res.status(400);
+    throw new Error('user not found');
+  }
+});
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -31,6 +51,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -39,7 +60,25 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route');
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('invalid credentials');
+  }
 });
 
-export { registerUser, loginUser };
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+export { getAllUsers, getSingleUser, registerUser, loginUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController.js';
+import { registerUser, loginUser, getAllUsers, getSingleUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
+router.get('/', getAllUsers)
+router.get('/:id', getSingleUser)
 router.post('/', registerUser);
 router.post('/login', loginUser);
 
