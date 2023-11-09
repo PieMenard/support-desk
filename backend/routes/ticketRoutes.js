71d165e0161cf4044a1201c0c507eac9648af46f
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-11-09 16:00:10 -0400

    get, update and delete ticket

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import protect from '../middleware/authMiddleware.js';
-import { getTickets, createTicket } from '../controllers/ticketController.js';
+import {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+} from '../controllers/ticketController.js';
 
 const ticketRouter = express.Router();
 
 ticketRouter.get('/', protect, getTickets);
 ticketRouter.post('/', protect, createTicket);
-
+ticketRouter.get('/:id', protect, getTicket);
+ticketRouter.delete('/:id', protect, deleteTicket);
+ticketRouter.put('/:id', protect, updateTicket);
 export default ticketRouter;
