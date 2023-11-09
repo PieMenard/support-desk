58e5442b3ac3a2e8c0ca1f5af6882a8255dac2bf
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-11-09 13:39:11 -0400

    finish get ticket and create ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -3,11 +3,40 @@ import User from '../models/userModel.js';
 import Ticket from '../models/ticketModel.js';
 
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'get tickets' });
+  //get user from token
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error('user not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'create ticket' });
+  const { product, description, status } = req.body;
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('please add product and description');
+  }
+
+  //get user from token
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error('user not found');
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status,
+  });
+
+  res.status(200).json(ticket);
 });
 
 export { getTickets, createTicket };
