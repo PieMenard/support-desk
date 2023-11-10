0333de713bb5457a36a8703ba76d2dc9bdda18b4
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-11-10 12:59:14 -0400

    add tickets to redux

---
+++ frontend/src/features/tickets/ticketService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = '/api/tickets/';
+
+//Register user
+const create= async (userData) => {
+ 
+};
+
+
+const authService = { create };
+
+export default authService;
